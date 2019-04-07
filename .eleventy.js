517a052862ba212aec6229d0382bbe2819e114ab
James Doc <hi@jamesdoc.com>
2019-04-07 22:22:05 +0100

    Add sitemap

--- .eleventy.js
+++ .eleventy.js
@@ -9,6 +9,10 @@ module.exports = function (eleventyConfig) {
     return DateTime.fromJSDate(dateObj).toFormat("dd LLLL yyyy");
   });
 
+  eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
+  });
+
   eleventyConfig.addFilter("dateComparison", (dateObj, compareWith)=> {
     return DateTime.fromJSDate(dateObj) < DateTime.fromISO(compareWith)
   })
