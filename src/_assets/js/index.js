77f21fc394efe37ba38dbbab8cd013de58375086
James Doc <hi@jamesdoc.com>
2020-05-16 17:09:46 +0100

    Get JS building

---
+++ src/_assets/js/index.js
@@ -0,0 +1,4 @@
+console.log('Oh, hai there.');
+console.log('There\'s not much in JSland, but feel free to go and poke around');
+console.log('All the code is on GitHib. Go have a poke around');
+console.log('https://github.com/jamesdoc/jamesdoc.com');
