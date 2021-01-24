e6b9018a4ef880c0773da6e6ed87d5a1d516ec7b
James Doc <hi@jamesdoc.com>
2021-01-24 19:11:41 +0000

    Paginate thoughts

--- utils/collections.js
+++ utils/collections.js
@@ -1,7 +1,22 @@
-module.exports = {
+const dayjs = require('dayjs');
 
-  posts: (collection) => {
-    return collection.getFilteredByGlob(["./src/blog/**/*.md"]);
-  },
+function getByDate(collection, dateFormat) {
+  let postsByDate = {};
+  let posts = collection.getFilteredByGlob(["./src/blog/**/*.md"]);
+  posts.forEach(function(post) {
+    // Skip the iteration is there is no date
+    if (!post.data.date) { return; }
+    let d = dayjs(post.data.date).format(dateFormat)
+    if (!postsByDate[d]) { postsByDate[d] = new Array(); }
+    postsByDate[d].push(post);
+  });
+  return postsByDate;
+}
 
+exports.postsByYear = collection => {
+  return getByDate(collection, 'YYYY');;
+}
+
+exports.posts = collection => {
+  return collection.getFilteredByGlob(["./src/blog/**/*.md"]);
 }
