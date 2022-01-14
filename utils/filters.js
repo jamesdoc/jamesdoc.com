6003b2ed37c37a1a3de57166b91610565c7ceff2
James Doc <hi@jamesdoc.com>
2022-01-14 17:59:34 +0000

    Filter and tidy the tag page

--- utils/filters.js
+++ utils/filters.js
@@ -1,13 +1,12 @@
-const dayjs = require('dayjs');
+const dayjs = require("dayjs");
 
 module.exports = {
-
   readableDate: (dateObj) => {
-    return dayjs(dateObj).format('dddd, D MMMM YYYY');
+    return dayjs(dateObj).format("dddd, D MMMM YYYY");
   },
 
   htmlDateString: (dateObj) => {
-    return dayjs(dateObj).format('YYYY-MM-DD');
+    return dayjs(dateObj).format("YYYY-MM-DD");
   },
 
   dateComparison: (dateObj, compareWith) => {
@@ -15,14 +14,26 @@ module.exports = {
   },
 
   imgSuffix: (imgStr, suffix) => {
-    const i = imgStr.lastIndexOf('.');
+    const i = imgStr.lastIndexOf(".");
     const imgPath = imgStr.substring(0, i);
     const ext = imgStr.substring(i + 1);
     return `${imgPath}-${suffix}.${ext}`;
   },
 
   activeBook: (bookList) => {
-    return bookList.filter(item => item.active);
+    return bookList.filter((item) => item.active);
   },
 
+  tagFilter: (posts, ignore) => {
+    const returnPosts = {};
+    const keys = Object.keys(posts);
+
+    keys.forEach((key, index) => {
+      if (ignore.indexOf(key) === -1) {
+        returnPosts[key] = posts[key];
+      }
+    });
+
+    return returnPosts;
+  },
 };
