bc95bf6a94a6a3ba38b8713b409aeba77fac71c2
James Doc <hi@jamesdoc.com>
2023-02-09 17:08:53 +0000

    Add now playing to now page

--- src/_assets/js/index.js
+++ src/_assets/js/index.js
@@ -1,4 +1,20 @@
 console.log('Oh, hai there.');
 console.log('There\'s not much in JSland, but feel free to go and poke around');
-console.log('All the code is on GitHib. Go have a poke around');
+console.log('All the code is on GitHub. Go have a poke around');
 console.log('https://github.com/jamesdoc/jamesdoc.com');
+
+
+const nowPlaying = document.querySelector('.js-nowPlaying');
+
+if (nowPlaying) {
+    const path = `https://recentfm.rknight.me/now.php?username=jamesdoc`
+    fetch(path)
+    .then((response) => response.json())
+    .then((data) => {
+        nowcontainer = document.createElement('div')
+        nowcontainer.className = 'recent-played'
+        nowcontainer.style
+        nowcontainer.innerHTML = `The most recent track scrobbled to Last.Fm was ${data.content}.`
+        nowPlaying.appendChild(nowcontainer)
+    })
+}
